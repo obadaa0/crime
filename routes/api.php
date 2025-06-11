d8f59d6aaea9e0c10987b8f26a4ab0b464f690fb
Obada Sabbagh <badasabbagh@gmail.com>
2025-06-11 10:18:43 +0300

    f

--- routes/api.php
+++ routes/api.php
@@ -21,10 +21,6 @@ use Illuminate\Support\Facades\Route;
     Route::get('/user/show-post/{user}',[UserController::class,'showpost']);
     Route::post('/user/profile-image',[UserController::class,'editProfile']);
     Route::put('/password/reset-in-profile',[PasswordResetController::class,'editPasswordInProfile']);
-    Route::get('/police/users',[UserController::class,'getUsers']);
-    Route::get('/police/polices',[UserController::class,'getPolice']);
-    Route::get('/user/block/{user}',[UserController::class,'blockUser']);
-    Route::get('/user/unblock/{user}',[UserController::class,'UnblockUser']);
     //post
     Route::post('/post/create',[PostController::class,'create']);
     Route::delete('/post/delete/{post}',[PostController::class,'delete']);
@@ -55,14 +51,9 @@ use Illuminate\Support\Facades\Route;
     Route::get('notification/number-of-nitif',[NotificationController::class,'numberOfNotification']);
     //report
     Route::get('/report/show',[ReportController::class,'show']);//->middleware('isPolice')
-    // Route::get('/report/show',[ReportController::class,'show']);//->middleware('isPolice')
     Route::post('/report/create',[ReportController::class,'create']);
     Route::get('report/progress/{report}',[ReportController::class,'setProgress']);
     Route::get('report/resolved/{report}',[ReportController::class,'setResolved']);
-    //admin
-    Route::post('/admin/police/create',[UserController::class,'createPolice']);
-    Route::post('admin/police/update/{user}',[UserController::class,'updatePolice']);
-    Route::delete('admin/police/delete/{user}',[UserController::class,'deletePolice']);
     //news
     Route::get('/news/show',[PostController::class,'summarizeNews']);
     //search
